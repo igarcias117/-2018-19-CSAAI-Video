d27c4175337f996e432443ce14a4547cd01a4814
Nacho Garcia Cano <igarcia@alpha06.aulas.gsyc.urjc.es>
2019-04-04 12:15:05 +0200

    videos reproducidos

--- video.js
+++ video.js
@@ -5,13 +5,30 @@ function main()
   parrafo = document.getElementById('parrafo')
   video1 = document.getElementById("video1")
 
-/*  parrafo.onmouseover = () => {
-    console.log("Mouse over!!")
-    parrafo.style.backgroundColor = "red"
+//video 1
+  video1.onmouseover = () => {
+    video1.muted = false;
   }
 
-  parrafo.onmouseout = () => {
-    console.log("Mouse out!!")
-    parrafo.style.backgroundColor = ""
-  } */
+  video1.onmouseout = () => {
+    video1.muted = true;
+  }
+
+//video 2
+  video2.onmouseover = () => {
+    video2.muted = false;
+  }
+
+  video2.onmouseout = () => {
+    video2.muted = true;
+  }
+
+//video 3
+  video3.onmouseover = () => {
+    video3.muted = false;
+  }
+
+  video3.onmouseout = () => {
+    video3.muted = true;
+  }
 }
