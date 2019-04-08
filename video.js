f1ba2fb539deb6ccb271b619d8b149f848c6ac74
Nacho Garcia Cano <igarcia@alpha10.aulas.gsyc.urjc.es>
2019-04-08 16:31:05 +0200

    VIDEOS A REPRODUCIR

--- video.js
+++ video.js
@@ -4,6 +4,12 @@ function main()
 
   parrafo = document.getElementById('parrafo')
   video1 = document.getElementById("video1")
+  video2 = document.getElementById("video2")
+  video3 = document.getElementById("video3")
+  video_principal = document.getElementById("video_principal")
+  boton1 = document.getElementById("boton1")
+  boton2 = document.getElementById("boton2")
+  boton3 = document.getElementById("boton3")
 
 //video 1
   video1.onmouseover = () => {
@@ -31,4 +37,21 @@ function main()
   video3.onmouseout = () => {
     video3.muted = true;
   }
+
+//video video_principal
+  boton1.onclick = () => {
+     video_principal.src = "Halo4.mp4";
+     video_principal.currentTime = video1.currentTime;
+   }
+
+  boton2.onclick = () => {
+     video_principal.src = "OP.mp4";
+     video_principal.currentTime = video2.currentTime;
+   }
+
+  boton3.onclick = () => {
+     video_principal.src = "anillo.mp4";
+     video_principal.currentTime = video3.currentTime;
+  }
+
 }
